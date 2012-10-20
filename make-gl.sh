1859e2f10ea413b94d0c4a59a0a2e8654b3ef2fe
pelya <x.pelya.x@gmail.com>
2012-10-21 00:03:34 +0300

    Updated makefile

--- make-gl.sh
+++ make-gl.sh
@@ -8,4 +8,4 @@ make -j8 release V=1 \
 USE_LOCAL_HEADERS=0 \
 USE_OPENAL=0 USE_CURL=1 USE_CURL_DLOPEN=0 USE_CODEC_VORBIS=1 USE_MUMBLE=0 USE_FREETYPE=1 \
 USE_RENDERER_DLOPEN=0 USE_INTERNAL_ZLIB=0 USE_INTERNAL_JPEG=1 && \
-cd build/release-linux-x86_64 && ./openarena.x86_64 +devmap kaos +set r_fullscreen 0
+cd build/release-linux-x86_64 && ./openarena.x86_64 +devmap kaos2 +set r_fullscreen 0
