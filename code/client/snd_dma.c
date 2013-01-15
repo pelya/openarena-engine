fa5eda2238a4b1ce2aa7797f154f80ce0a95c00c
pelya <x.pelya.x@gmail.com>
2013-01-16 00:48:34 +0200

    Disabled warning about music format

--- code/client/snd_dma.c
+++ code/client/snd_dma.c
@@ -1402,7 +1402,7 @@ void S_Base_StartBackgroundTrack( const char *intro, const char *loop ){
 	}
 
 	if(s_backgroundStream->info.channels != 2 || s_backgroundStream->info.rate != 22050) {
-		Com_Printf(S_COLOR_YELLOW "WARNING: music file %s is not 22k stereo\n", intro );
+		//Com_Printf(S_COLOR_YELLOW "WARNING: music file %s is not 22k stereo\n", intro ); // Warning spawns too often
 	}
 }
 
